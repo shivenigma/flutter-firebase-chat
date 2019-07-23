533339721281cb9f9a6fb4aaeb64951ad5d99c7c
Vignesh M <vigneshm@agiratech.com>
2019-07-24 01:03:51 +0530

    Learnt about Builder method, Scaffold context, Snack bar
    Firebase error handling

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -21,79 +21,110 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: ModalProgressHUD(
-        inAsyncCall: isLoading,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Flexible(
-                child: Hero(
-                  tag: 'logo',
-                  child: Container(
-                    height: 200.0,
-                    child: Image.asset('images/logo.png'),
+      body: Builder(builder: (BuildContext context) {
+        return ModalProgressHUD(
+          inAsyncCall: isLoading,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Flexible(
+                  child: Hero(
+                    tag: 'logo',
+                    child: Container(
+                      height: 200.0,
+                      child: Image.asset('images/logo.png'),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 48.0,
-              ),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                textCapitalization: TextCapitalization.none,
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration:
-                    kTextFiledDecoration.copyWith(hintText: 'Enter your Email'),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              TextField(
-                obscureText: true,
-                textCapitalization: TextCapitalization.none,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: kTextFiledDecoration.copyWith(
-                    hintText: 'Enter your password'),
-              ),
-              SizedBox(
-                height: 24.0,
-              ),
-              ActionButton(
-                color: Colors.lightBlueAccent,
-                label: Text(
-                  'Log In',
-                  style: TextStyle(color: Colors.white),
+                SizedBox(
+                  height: 48.0,
                 ),
-                onPressed: () async {
-                  setState(() {
-                    isLoading = true;
-                  });
-                  try {
-                    final user = await _auth.signInWithEmailAndPassword(
-                        email: email, password: password);
-                    if (user != null) {
-                      Navigator.pushNamed(context, ChatScreen.route);
-                    }
-                  } catch (e) {
-                    print(e);
-                  } finally {
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  textCapitalization: TextCapitalization.none,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration:
+                  kTextFiledDecoration.copyWith(hintText: 'Enter your Email'),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  obscureText: true,
+                  textCapitalization: TextCapitalization.none,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: kTextFiledDecoration.copyWith(
+                      hintText: 'Enter your password'),
+                ),
+                SizedBox(
+                  height: 24.0,
+                ),
+                ActionButton(
+                  color: Colors.lightBlueAccent,
+                  label: Text(
+                    'Log In',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
                     setState(() {
-                      isLoading = false;
+                      isLoading = true;
                     });
-                  }
-                },
-              ),
-            ],
+                    try {
+                      final user = await _auth.signInWithEmailAndPassword(
+                          email: email, password: password);
+                      if (user != null) {
+                        if(user.displayName != null) {
+                          Navigator.pushNamed(context, ChatScreen.route);
+                        } else {
+                          Navigator.pushNamed(context, ProfileScreen.route);
+                        }
+                      }
+                    } catch (e) {
+                      print(e.code);
+                      var authError;
+                      switch (e.code) {
+                        case 'ERROR_INVALID_EMAIL':
+                          authError = 'Invalid Email';
+                          break;
+                        case 'ERROR_USER_NOT_FOUND':
+                          authError = 'User Not Found';
+                          break;
+                        case 'ERROR_WRONG_PASSWORD':
+                          authError = 'Wrong Password';
+                          break;
+                        default:
+                          authError = 'Something went wrong, Please try later';
+                          break;
+                      }
+                      final snackBar = SnackBar(
+                        content: Text(authError),
+                        action: SnackBarAction(
+                          label: 'OK',
+                          onPressed: () {
+                            Scaffold.of(context).hideCurrentSnackBar();
+                          },
+                        ),
+                      );
+                      Scaffold.of(context).showSnackBar(snackBar);
+                    } finally {
+                      setState(() {
+                        isLoading = false;
+                      });
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
-        ),
-      ),
+        );
+      })
     );
   }
 }
